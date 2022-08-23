4f1dfedc38e565c882473579b3783324f41f5d92
jemrel mangaliman <jemrelricky.mangaliman@cvsu.edu.ph>
2022-08-23 20:08:32 +0800

    initial progress

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="bootstrap-4.0.0-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <!-- Navigation bar at the top page -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- Brand logo -->
+        <span class="navbar-brand h1">Shopr</span>
+        <!-- Login and Sign up links-->
+        <div class="container-fluid" id="LoginSignup">
+            <ul class="nav" >
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sign Up</a>
+                </li>
+                <li class="nav-item">
+                    <button class="btn btn-outline-success" id="buttonlogin">Login</button>
+                </li>
+
+            </ul>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row" id="landingcontent">
+            <div class="col-lg" id="landingcontent-firstcolumn">
+
+            </div>
+            <div class="col-lg" id="landingcontent-secondcolumn">
+                <h1 id="tagline">Easy Shopping? Do it with Shopr.</h1>
+                <p id="landingcontent-smalldescription">Shopping has never been easy as Shopr bring different products closer to you! Shop easy in three steps: Add to cart, Check out, Enjoy.</p>
+            </div>
+    </div>
+    </div>
+
+
+
+<script src="bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
+<?php
+?>
+
